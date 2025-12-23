e535ee7bcd739080a490b6d1bc955f293cbdd77d
dhanarajcse <dhanarajcse@gmail.com>
2025-12-23 23:51:40 +0530

    Update employeedesk-app.py

--- employeedesk-app.py
+++ employeedesk-app.py
@@ -1,4 +1,15 @@
 import streamlit as st
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 st.title("EmployeeDesk")
-st.success("Streamlit app created successfully!")
+
+try:
+    uri = st.secrets["MONGODB_URI"]
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    client.admin.command("ping")
+
+    st.success("Connected to MongoDB Atlas successfully!")
+
+except Exception as e:
+    st.error(f"MongoDB connection failed: {e}")
