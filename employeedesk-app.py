9996152c3af89e565ce2ecb2a875ca55ed6fb803
dhanarajcse <dhanarajcse@gmail.com>
2025-12-23 22:26:51 +0530

    Add files via upload

---
+++ employeedesk-app.py
@@ -0,0 +1,24 @@
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://admin-dev:YOUR_PASSWORD@employeedesk-cluster.gbjyes6.mongodb.net/db_employeedesk?appName=employeedesk-cluster"
+
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+try:
+    client.admin.command('ping')
+    print("Connected successfully!")
+
+    db = client["db_employeedesk"]
+
+    db.employees.insert_one({
+        "employee_id": "EMP001",
+        "name": "First Employee",
+        "department": "IT"
+    })
+
+    print("Database and collection created!")
+
+except Exception as e:
+    print(e)
